0fdd7cdedad58b74e2506b48ea71ddf9b8b963ed
RUTAAA <82164338+RUTAAA@users.noreply.github.com>
2022-03-09 20:36:16 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+/* CA VA CHERCHER :root ET LE DEFINI COMME UNE VARIABLE NOMMEE r */
+var r = document.querySelector(':root');
+
+/* J'EN AI CHIE MAIS CA FONCTIONNE !!! */
+function setVoletWidth() {
+  var rs = getComputedStyle(r);
+  if (rs.getPropertyValue('--width_volet') == 0) {
+    r.style.setProperty('--width_volet', "360px");
+    r.style.setProperty('--open_top', "-100px");
+    r.style.setProperty('--close_top', "0");
+  } else {
+    r.style.setProperty('--width_volet', 0);
+    r.style.setProperty('--open_top', "0");
+    r.style.setProperty('--close_top', "-100px");
+  }
+}
+
+function setTheme() {
+  var rs = getComputedStyle(r);
+  if (rs.getPropertyValue('--fond') == "#24323F") {
+    r.style.setProperty('--fond', "#F2D492");
+    r.style.setProperty('--bandeau', "#F29559");
+    r.style.setProperty('--boutons', "#18212B");
+    r.style.setProperty('--dark_top', "-100px");
+    r.style.setProperty('--light_top', "0");
+  } else {
+    r.style.setProperty('--fond', "#24323F");
+    r.style.setProperty('--bandeau', "#18212B");
+    r.style.setProperty('--boutons', "#F29559");
+    r.style.setProperty('--dark_top', "0");
+    r.style.setProperty('--light_top', "-100px");
+  }
+}
