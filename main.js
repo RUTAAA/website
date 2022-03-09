636e7404413af75babaf782bc6520165a63cc851
RUTAAA <82164338+RUTAAA@users.noreply.github.com>
2022-03-09 20:55:24 +0100

    Update main.js

--- main.js
+++ main.js
@@ -18,16 +18,24 @@ function setVoletWidth() {
 function setTheme() {
   var rs = getComputedStyle(r);
   if (rs.getPropertyValue('--fond') == "#24323F") {
+    light();
+  } else {
+    dark();
+  }
+}
+
+function light() {
     r.style.setProperty('--fond', "#F2D492");
     r.style.setProperty('--bandeau', "#F29559");
     r.style.setProperty('--boutons', "#18212B");
     r.style.setProperty('--dark_top', "-100px");
     r.style.setProperty('--light_top', "0");
-  } else {
+}
+
+function dark() {
     r.style.setProperty('--fond', "#24323F");
     r.style.setProperty('--bandeau', "#18212B");
     r.style.setProperty('--boutons', "#F29559");
     r.style.setProperty('--dark_top', "0");
     r.style.setProperty('--light_top', "-100px");
-  }
 }
